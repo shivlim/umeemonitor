9d3ae6b5d95342395456cb59f4e6d508d177b1a5
7029732+shivlim@users.noreply.github.com <7029732+shivlim@users.noreply.github.com>
2022-03-21 23:23:06 +0000

    Added jounrnactl

--- journalctl.js
+++ journalctl.js
@@ -1,6 +1,14 @@
-const Journalctl = require('journalctl');
-//journalctl -u peggod  --since "1 minutes ago"
-const journalctl = new Journalctl({'unit':'axelarmonitor','since':'1000 minutes ago'});
-journalctl.on('event', (event) => {
-    console.log("event")
+const { exec } = require("child_process");
+
+exec("ls -la", (error, stdout, stderr) => {
+    if (error) {
+        console.log(`error: ${error.message}`);
+        return;
+    }
+    if (stderr) {
+        console.log(`stderr: ${stderr}`);
+        return;
+    }
+    console.log(`stdout: ${stdout}`);
 });
+
